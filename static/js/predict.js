14a5e00c04e80e266f4c1da9ab9f337ac1d47a5a
Param Dhama <dhamaparam@gmail.com>
2023-08-17 14:31:44 +0530

    Create predict.js

---
+++ static/js/predict.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('#prediction-form').submit(function(event) {
+        event.preventDefault();
+        var selectedModel = $('#model-select').val();
+        $.ajax({
+            url: '/predict',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                model: selectedModel,
+                features: [
+                    parseFloat($('#sepal-length').val()),
+                    parseFloat($('#sepal-width').val()),
+                    parseFloat($('#petal-length').val()),
+                    parseFloat($('#petal-width').val())
+                ]
+            }),
+            success: function(response) {
+                $('#prediction-result').text('Predicted Class: ' + response.prediction);
+            }
+        });
+    });
+});
